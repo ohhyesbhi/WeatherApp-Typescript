9601661a91301a8d7997ecd756a3adad9a6c4c14
unknown <kamatabhishek14@gmail.com>
2023-10-18 09:18:20 +0530

    Added all the data in created state

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import {useEffect} from "react"
 import './App.css'
 import Home from './pages/Home'
 import { fetchData } from "./redux/slices/ForecastSlice"
-import { useDispatch } from "react-redux/es/exports"
 import { useAppDispatch } from "./hooks/hooks"
 
 
 function App() {
   const dispatch = useAppDispatch();
+
  useEffect(()=>{
    console.log(dispatch(fetchData()))
  },[])
-  return (
+  
+ return (
     <>
     <Home/>
     </>
--- src/redux/slices/ForecastSlice.tsx
+++ src/redux/slices/ForecastSlice.tsx
@@ -8,6 +8,17 @@ interface DayForecast{
    conditional : string
  }
 
+ interface DayForecast1{
+    date : string,
+    day :{
+    avgtemp_c : number,
+    avgtemp_f : number,
+    condition : {
+        text : string
+    }
+    }
+  }
+
  interface currentDayForecast{
     uv : number,
     wind_kmph : number,
@@ -37,19 +48,40 @@ interface ForecastData{
 
 interface ForecastDataState{
 status : "default"|"loading"|"success"|"failure",
-data : ForecastData | undefined
+data : ForecastData 
 }
 
 const initialState:ForecastDataState = {
     status:'default',
-    data:undefined,
+    data:{
+        location:{
+            country:'',
+            region:'',
+            name:'',
+            localtime:''
+        },
+        dayforecast:[],
+        currentData:{
+            uv : 0,
+            wind_kmph : 0,
+            humidity : 0,
+            vis_km : 0,
+            aqi : 0,
+            sunrise : '',
+            sunset : '',
+            temp_c : 0,
+            temp_f : '',
+            condition : '',
+            is_day : false,
+            chance_of_rain : 0
+        }
+    },
 }
 
 
 export const fetchData  = createAsyncThunk('data/fetchdata',async()=>{
     try {
       const response = await axiosInstance.get(`forecast.json?key=${import.meta.env.VITE_API_KEY}&days=7&aqi=yes&q=bengaluru`)  ;
-      console.log(response)
       return response
     } catch (error) {
         console.log(error)
@@ -62,8 +94,52 @@ const forecastslice = createSlice({
     reducers:{},
     extraReducers : (builder)=>{ 
         builder.addCase(fetchData.fulfilled,(state,action)=>{
-                   if(!action.payload) return
-                   
+                   if(action.payload == undefined) return;
+                   console.log(action.payload.data,"current")
+                 
+                   const {location} = action.payload.data;
+                   const foreCast = action.payload.data.forecast.forecastday;
+                   const currentData = action.payload.data.current
+
+                   state.data.location.name = location?.name;
+                   state.data.location.country = location?.country
+                   state.data.location.region = location?.region
+                   state.data.location.localtime = location?.localtime 
+
+                   state.data.dayforecast = foreCast.map((data:DayForecast1)=>{
+                    return{
+                        date : data.date,
+                        avgtemp_c : data.day.avgtemp_c,
+                        avgtemp_f : data.day.avgtemp_f,
+                        condition : data.day.condition.text
+                    }
+                   })
+
+                   state.data.currentData= {
+                    uv : currentData.uv,
+                    wind_kmph : currentData.wind_kph,
+                    humidity : currentData.humidity,
+                    vis_km : currentData.vis_km                    ,
+                    aqi : currentData.air_quality,
+                    sunrise : foreCast[0].astro.sunrise,
+                    sunset : foreCast[0].astro.sunset,
+                    temp_c : currentData.temp_c,
+                    temp_f : currentData.temp_f,
+                    condition : currentData.condition.text,
+                    is_day : currentData.is_day,
+                    chance_of_rain : foreCast[0].day.condition.text
+                   }
+
+
+
+        })
+        .addCase(fetchData.pending,(state,action)=>{
+            state.status = "loading"
+            console.log(action)
+        })
+        .addCase(fetchData.rejected,(state,action)=>{
+            state.status = "failure"
+            console.log(action)
         })
     }
 })
